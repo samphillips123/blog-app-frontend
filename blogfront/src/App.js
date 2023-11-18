0ca8b890037cb0133d990ea3fccb5ebdeefe7c42
samphillips123 <sphillips123@gmail.com>
2023-11-18 13:32:02 -0800

    set up routes in App.js

--- blogfront/src/App.js
+++ blogfront/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 // import components
-
+import AllPosts from './pages/AllPosts'
+import Form from './pages/Form'
+import SinglePost from './pages/SinglePost'
 // import hooks
 import {useState, useEffect } from 'react'
 // import react-router
@@ -13,6 +15,28 @@ function App() {
   return (
     <div className="App">
       <h1>Blog App</h1>
+      <Routes>
+        <Route 
+          exact
+          path='/'
+          element={<AllPosts />}
+        />
+        <Route 
+          exact
+          path='/blog/:id'
+          element={<SinglePost />}
+        />
+        <Route 
+          exact
+          path='/new'
+          element={<Form />}
+        />
+        <Route 
+          exact
+          path='/edit/:id'
+          element={<Form />}
+        />
+      </Routes>
     </div>
   );
 }
