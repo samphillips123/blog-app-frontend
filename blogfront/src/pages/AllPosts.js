6d2c064608ef9ef9774499738fe11f369f377e03
samphillips123 <sphillips123@gmail.com>
2023-11-21 16:42:12 -0800

    set up form to use add and edit

--- blogfront/src/pages/AllPosts.js
+++ blogfront/src/pages/AllPosts.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom'
 
 const AllPosts = (props) => (
     <>
+        <Link to={'/new'}>
+            <button>Add Blog Post</button>
+        </Link>
         {props.posts.map(
             (post) => <Post post={post} key={post.id} />
         )}
--- blogfront/src/pages/Form.js
+++ blogfront/src/pages/Form.js
@@ -1,5 +1,58 @@
+import { useMemo, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+
 const Form = (props) => {
-    return <h1>Form</h1>
+    const navigate = useNavigate()
+    const params = useParams()
+
+    // Get current post for edit
+    const currentPost = useMemo(() => props.posts.find(post => post.id === parseInt(params.id)), [params.id, props.posts])
+
+    const [formData, setFormData] = useState(
+        props.formType === 'new' ? {
+            title: '',
+            body: ''
+        } : { // if editing, will pull in exisiting data
+            title: currentPost.title,
+            body: currentPost.body,
+            id: parseInt(currentPost.id)
+        }
+    )
+
+    const handleChange = (event) => {
+        setFormData((prev) => (
+            {
+                ...prev,
+                [event.target.name]: event.target.value
+            }
+        ))
+    }
+
+    const handleSubmission = (event) => {
+        event.preventDefault()
+        props.handleSubmit(formData, props.formType)
+        navigate('/')
+    }
+
+    return (
+        <form onSubmit={handleSubmission}>
+            <h3>Title</h3>
+            <input
+                type='text'
+                onChange={handleChange}
+                value={formData.title}
+                name='title'
+            />
+            <h3>Body</h3>
+            <input
+                type='text'
+                onChange={handleChange}
+                value={formData.body}
+                name='body'
+            />
+            <input type='submit' value={props.buttonLabel} />
+        </form>
+    )
 }
 
 export default Form
